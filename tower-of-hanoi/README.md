7d3c8b18b7b921e9e1196307c21476bca1ab6a45
Tishana <tishana.trainor@gmail.com>
2018-10-24 12:28:27 -0400

    basic logic set up

--- tower-of-hanoi/README.md
+++ tower-of-hanoi/README.md
@@ -1,1 +1,36 @@
 # tower-of-hanoi
+/*
+create variables for all elements:
+all 3 poles
+all disks
+
+all poles can have a maximum of 6 disks, minimum of 0.
+
+all disks on poles must stack in size order, smallest on top, largest on bottom
+
+move selected disk to selected pole:
+<select disk>
+user clicks disk
+<select new pole>
+user clicks pole where disk will go
+<move disk to pole>
+if there is no disk on selected pole, disk appears on pole
+if selected disk is larger than disk on selected pole(check the width attribute of both), alert "Nope, can't do that!"
+
+when all disks are present on third pole, player wins, (alert "You win!")
+
+*/
+
+/*
+Game Functions:
+
+When a tower (sending div) is clicked, browser waits for a second click on a new tower(receiving div).
+
+If there is a disk (moving disk) in receiving div, it is compared to the topmost disk from the sending div. 
+
+If moving disk is larger than the topmost disk in the receiving div, a move IS NOT allowed.
+
+If moving disk is smaller than the topmost disk, a move IS allowed.
+
+Reset button places all disks in first div.
+*/
--- tower-of-hanoi/main.js
+++ tower-of-hanoi/main.js
@@ -1,57 +1,17 @@
 /*
-create variables for all elements:
-all 3 poles
-all disks
-
-all poles can have a maximum of 6 disks, minimum of 0.
-
-all disks on poles must stack in size order, smallest on top, largest on bottom
-
-move selected disk to selected pole:
-<select disk>
-user clicks disk
-<select new pole>
-user clicks pole where disk will go
-<move disk to pole>
-if there is no disk on selected pole, disk appears on pole
-if selected disk is larger than disk on seleced pole(check the width attribute of both), alert "Nope, can't do that!"
-
-when all disks are present on third pole, player wins, (alert "You win!")
-
+see readme.md file for game function
 */
+inPlay = false // this determines is a disk has already been selected
+oldChild = undefined // this will be the disk being moved to another tower
+let p1 = document.querySelector("#poleOne") // first pole
+let p2 = document.querySelector("#poleTwo") // second pole
+let p3 = document.querySelector("#poleThree") //third pole
+let reset = document.querySelector(".reset") // reset button
 
-/*
-Game Functions:
-
-When a tower (sending div) is clicked, browser waits for a second click on a new tower(receiving div).
-
-If there is a disk (moving disk) in receiving div, it is compared to the topmost disk from the sending div. 
-
-If moving disk is larger than the topmost disk in the receiving div, a move IS NOT allowed.
-
-If moving disk is smaller than the topmost disk, a move IS allowed.
-
-Reset button places all disks in first div.
-*/
-
-let d1 = document.querySelector("#one");
-let d2 = document.querySelector("#two")
-let d3 = document.querySelector("#three");
-let d4 = document.querySelector("#four");
-let d5 = document.querySelector("#five");
-let d6 = document.querySelector("#six")
-
-let p1 = document.querySelector("#poleOne")
-let p2 = document.querySelector("#poleTwo")
-let p3 = document.querySelector("#poleThree")
-let reset = document.querySelector(".reset")
-
-
-
-addDisks = function (tower) {//adding disk to receiving tower
+addDisks = (tower) => {//adding disk to receiving tower
 
 }
-compareDisks = function () {//compare sizes of disks being stacked
+compareDisks = () => {//compare sizes of disks being stacked
     // if (tower1.)
 }
 clearDisk = function (sTower, rTower) {// remove top disk from sending tower
@@ -62,6 +22,13 @@ clearDisk = function (sTower, rTower) {// remove top disk from sending tower
     // tower.removeChild(oldChild);
     console.log("help")
 }
+moveDisks = function () {
+    let these = p3.querySelectorAll(".disk")
+    let those = p2.querySelectorAll(".disk")
+    if (these[0].attributes.class.ownerElement.clientWidth < those[0].attributes.class.ownerElement.clientWidth) {
+        alert("Too big... ")
+    }
+}
 
 let disks = document.querySelectorAll(".disk")
 resetGame = function (evt) {
@@ -71,37 +38,90 @@ resetGame = function (evt) {
     // disks.forEach((ele) => {
     //     ele.setAttribute("parentNode", "div#p1")
     // }//move to first tower (poleOne)
-    alert("OMG, I'm clicked already!")//to test 
+    // alert("OMG, I'm clicked already!")//to test 
 }
 
 reset.addEventListener('click', resetGame)
 
 
 p1.addEventListener('click', function () {// remove top disk from sending tower
-    if (p1.querySelectorAll(".disk").length > 0) {
-        // if (p1.querySelectorAll[0].attributes.class.ownerElement.clientWidth > p2.querySelectorAll[0].attributes.class.ownerElement.clientWidth) {
-        let oldChild = p1.removeChild(p1.querySelectorAll(".disk")[0])
-        p2.appendChild(oldChild)
-        // } else ("Nope, can't put a larger disk on a smaller disk...")
+    if (p1.querySelectorAll(".disk").length > 0 && inPlay === false) {
+        oldChild = p1.querySelectorAll(".disk")[0]
+        oldChild.style.backgroundColor = "red"
+        inPlay = true
+    } else if (inPlay === true && p1.childElementCount == 0) {
+        p1.appendChild(oldChild)
+        oldChild.style.backgroundColor = "#e0f2e9"
+        oldChild = undefined
+        inPlay = false
+    } else if ((inPlay === true) && (oldChild.clientWidth < p1.children[0].clientWidth)) {
+        p1.insertBefore(oldChild, p1.children[0])
+        oldChild.style.backgroundColor = "#e0f2e9"
+        oldChild = undefined
+        inPlay = false
     } else {
-        alert("No more disks on this pole...")
+        alert("Nah, son...")
     }
 })
 
 p2.addEventListener('click', function () {
+
+    if (p2.querySelectorAll(".disk").length > 0 && inPlay === false) {
+        oldChild = p2.querySelectorAll(".disk")[0]
+        oldChild.style.backgroundColor = "red"
+        inPlay = true
+
+        // p3.insertBefore(oldChild, p3.children[0])//console.log(p2Div + " , " + 
+    } else if (inPlay === true && p2.childElementCount == 0) {
+        p2.appendChild(oldChild)
+        oldChild.style.backgroundColor = "#e0f2e9"
+        oldChild = undefined
+        inPlay = false
+    } else if ((inPlay === true) && (oldChild.clientWidth < p2.children[0].clientWidth)) {
+        p2.insertBefore(oldChild, p2.children[0])
+        oldChild.style.backgroundColor = "#e0f2e9"
+        oldChild = undefined
+        inPlay = false
+    } else {
+        alert("Nah, son...")
+    }
+})
+
+p3.addEventListener('click', function () {
+    if (p3.querySelectorAll(".disk").length > 0 && inPlay === false) {
+        oldChild = p3.querySelectorAll(".disk")[0]
+        oldChild.style.backgroundColor = "red"
+        inPlay = true
+    } else if (inPlay === true && p3.childElementCount == 0) {
+        p3.appendChild(oldChild)
+        oldChild.style.backgroundColor = "#e0f2e9"
+        oldChild = undefined
+        inPlay = false
+    } else if ((inPlay === true) && (oldChild.clientWidth < p3.children[0].clientWidth)) {
+        p3.insertBefore(oldChild, p3.children[0])
+        oldChild.style.backgroundColor = "#e0f2e9"
+        oldChild = undefined
+        inPlay = false
+    } else {
+        alert("Nah, son...")
+    }
+})
+/* // *** OLD LOGIC- 
+when set to each tower, simply moves disks to the next tower, no rules. 
+p2.addEventListener('click', function () {
     if (p2.querySelectorAll(".disk").length > 0) {
         let oldChild = p2.removeChild(p2.querySelectorAll(".disk")[0])
-        p3.appendChild(oldChild)
+        // let p2Div = p2.children[0].attributes.class.ownerElement.clientWidth
+        // let p3Div = p3.children[0].attributes.class.ownerElement.clientWidth
+        // if (p2Div < p3Div) {
+        p3.insertBefore(oldChild, p3.children[0])//console.log(p2Div + " , " + p3Div)
+        // } else {
+        //     alert("Nope, illegal move, son...")
+        //     p2.appendChild(oldChild, p2.children)
+        // }
     } else {
         alert("No more disks on this pole...")
     }
 })
 
-p3.addEventListener('click', function () {
-    if (p3.querySelectorAll(".disk").length > 0) {
-        let oldChild = p3.removeChild(p3.querySelectorAll(".disk")[0])
-        p1.appendChild(oldChild)
-    } else {
-        alert("No more disks on this pole...")
-    }
-})
+*/
